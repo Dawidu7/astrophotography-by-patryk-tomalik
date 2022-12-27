07ec4a9678281acaf9949c2351542763f62abd3e
Dawidu7 <109083134+Dawidu7@users.noreply.github.com>
2022-12-27 20:52:55 +0100

    Color Change

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,8 +7,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        dark: '#242933',
-        'light-dark': '#2a303c',
+        // dark: '#242933',
+        dark: '#373842',
+        // 'light-dark': '#2a303c',
+        'light-dark': '#454650',
         light: '#cecece',
       },
     },
